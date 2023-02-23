07b97323cc26cac4b6c294f65585b43431a22877
Elie Benzaquen <eliebenzaquen@gmail.com>
2023-02-22 23:02:30 -0500

    created spring boot project

---
+++ distbox/src/main/java/com/capstone/distbox/DistboxApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.distbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistboxApplication.class, args);
+	}
+
+}
---
+++ distbox/src/test/java/com/capstone/distbox/DistboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.distbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
